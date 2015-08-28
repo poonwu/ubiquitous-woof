4bca206073c7060333a8c3b80473b3e7da6508b8
Pat Sabpisal <ssabpisa@me.com>
2015-08-28 11:35:05 +0700

    hmm

--- defectdet.py
+++ defectdet.py
@@ -2,22 +2,30 @@ import cv2
 import math
 
 MAIN_WINDOW = "Onward"
-TILT_ANGLE = 30
+
+probe_file = 'messi5_defect.jpg'     # probe input
+base_file = 'messi5.jpg'             # base file is first acceptable input 
+
+# Thoughts: 
+# we take N samples of non-defects and M samples of defects
+# For N+M output we can use linear classifier to classify unseen samples into group of non-defects or defects
+#
 
 # Read image
-img0 = cv2.imread('gates_defect.jpg', 0)
-edges0 = cv2.Canny(img0,300,430)
+defect_img = cv2.imread(probe_file, 0)
+defect_edge = cv2.Canny(defect_img,100,200)
 
-img1 = cv2.imread('gates.jpg', 0)
-edges1 = cv2.Canny(img1,300,430)
+img1 = cv2.imread(base_file, 0)
+edges1 = cv2.Canny(img1,100,200)
 
-exor = edges0 ^ edges1
+exor = defect_edge ^ edges1 
 
 # create window
 cv2.namedWindow(MAIN_WINDOW)
 cv2.imshow(MAIN_WINDOW, exor)
 
 while(True):
+
 	pressed_key = cv2.waitKey(5)
 	if pressed_key != -1 and pressed_key not in [65365, 65366]:
 		break
